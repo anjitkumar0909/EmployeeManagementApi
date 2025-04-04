6a6f4a41cdaa42737b925ed71dc70cec42589612
anjitkumar0909 <anjit.sury1c11@gmail.com>
2025-04-04 13:06:07 +0530

    Initial Commit

---
+++ Employee_erp/Employee_erp/Data/Employee_erpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Employee_erp.Models;
+
+namespace Employee_erp.Data
+{
+    public class Employee_erpContext : DbContext
+    {
+        public Employee_erpContext (DbContextOptions<Employee_erpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee_erp.Models.Employee> Employee { get; set; } = default!;
+    }
+}
